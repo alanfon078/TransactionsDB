54a29ed571b12193ee82242226f569bab88d66e1
alanfon078 <ls23120218@surguanajuato.tecnm.mx>
2025-10-28 00:41:05 -0600

    Add files via upload

--- TransactionsDB.sql
+++ TransactionsDB.sql
@@ -1,4 +1,5 @@
 create database if not exists TransactionsDB;
+-- drop database TransactionsDB;
 USE TransactionsDB;
 
 create table if not exists product(
@@ -14,8 +15,17 @@ create table if not exists product(
 -- Insertar algunos datos de ejemplo (opcional)
 INSERT INTO product (codigoDeBarras, nombre, precio, stock) 
 VALUES
-('7501001111111', 'Refresco de Cola 600ml', 18.50, 100),
-('7501002222222', 'Papas Fritas Sal 50g', 15.00, 200),
-('7501003333333', 'Jabón de Barra Neutro', 12.00, 50);
+('0070847036043', 'Monster Zero Ultra', 38.00, 130),
+('7798422620304', 'Monster Ultra Strawberry Dreams', 38.00, 120),
+('070847898245', 'Monster Ultra Blue Hawaiian', 48.00, 110),
+('0070847894216', 'Monster Ultra Golden Pineapple', 38.00, 100),
+('070847034629', 'Monster Ultra Fiesta Mango', 48.00, 90),
+('070847027324', 'Monster Ultra Violet', 48.00, 80),
+('0070847897637', 'Monster Ultra Peachy Keen', 38.00, 70),
+('0070847894230', 'Monster Ultra Watermelon', 38.00, 60),
+('0070847036715', 'Monster Ultra Paradise', 38.00, 50),
+('070847812609', 'Java Monster Mean Bean', 60.00, 40),
+('070847812715', 'Java Monster Loca Moca', 60.00, 30),
+('070847898405', 'Monster Killer Brew Triple Shot Mean Bean', 80.00, 20);
 
 select * from product;
